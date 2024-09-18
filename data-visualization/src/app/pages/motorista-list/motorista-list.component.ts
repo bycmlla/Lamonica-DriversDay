5adbcc949a47dbe8db5130d1504fbf40d3fbd132
Camila Carvalho <110317945+bycmlla@users.noreply.github.com>
2024-09-18 15:04:07 -0300

    filtered

--- data-visualization/src/app/pages/motorista-list/motorista-list.component.ts
+++ data-visualization/src/app/pages/motorista-list/motorista-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Motorista } from '../../models/motorista';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-motorista-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './motorista-list.component.html',
   styleUrl: './motorista-list.component.scss',
 })
@@ -27,8 +28,8 @@ export class MotoristaListComponent implements OnInit {
   ];
   motoristas: Motorista[] = [
     {
-      idMot: 1,
-      nomeMot: 'João Silva',
+      idMot: 2,
+      nomeMot: 'roberto silva',
       supervisor: 'Carlos Oliveira',
       data: new Date(),
       inicioJornada: '08:00',
@@ -57,8 +58,8 @@ export class MotoristaListComponent implements OnInit {
       descanso: '15min',
     },
     {
-      idMot: 1,
-      nomeMot: 'João Silva',
+      idMot: 3,
+      nomeMot: 'camila aguiar',
       supervisor: 'Carlos Oliveira',
       data: new Date(),
       inicioJornada: '08:00',
@@ -70,10 +71,39 @@ export class MotoristaListComponent implements OnInit {
       horaExtra: '1h',
       refeicao: '30min',
       descanso: '15min',
-    }
-  ]
+    },
+    {
+      idMot: 3,
+      nomeMot: 'camila aguiar',
+      supervisor: 'Carlos Oliveira',
+      data: new Date(),
+      inicioJornada: '08:00',
+      fimJornada: '18:00',
+      repouso: '1h',
+      tempoTotalJornada: '10h',
+      conducao: '5h',
+      direcaoMaxContinua: '2h',
+      horaExtra: '1h',
+      refeicao: '30min',
+      descanso: '15min',
+    },
+  ];
+  filteredMotoristas: Motorista[] = this.motoristas;
+  filterValue: string = '';
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  buscarMotoristas(): void {
+    const nome = this.filterValue.trim().toLowerCase();
+
+    if (nome === '') {
+      this.filteredMotoristas = this.motoristas;
+    } else {
+      this.filteredMotoristas = this.motoristas.filter((motorista) =>
+        motorista.nomeMot.toLowerCase().includes(nome)
+      );
+    }
+  }
 }
